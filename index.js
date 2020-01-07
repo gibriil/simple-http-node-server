0c11bf8428772d940994a8b9bb3c1be884b22f3e
Peter Beard <peter.beard@uvu.edu>
2020-01-07 10:48:37 -0700

    Server now returns JSON

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ const app = http.createServer((req, res) => {
             res.end();
             break;
         case ('/api'):
+            res.setHeader('Content-Type', 'application/json');
+            const obj = {
+                key1: "value 1",
+                key2: "value 2",
+                key3: "value 3"
+            }
+            res.write(JSON.stringify(obj));
+            res.end();
             break;
     }
 });
